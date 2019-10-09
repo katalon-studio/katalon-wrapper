88d03db7a059749dbdf296b4d94f491acba876d8
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-10-09 10:04:34 +0700

    Download and Extract Katalon package

--- src/com/katalon/katalonwrapper/download/download.go
+++ src/com/katalon/katalonwrapper/download/download.go
@@ -1,55 +1,233 @@
 package download
 
 import (
-	"net/http"
+	"archive/zip"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
 	"runtime"
 	"strings"
 )
 
 const Releases = "https://raw.githubusercontent.com/katalon-studio/katalon-studio/master/releases.json"
 
-type KatalonVersion struct{
+func handleErrorIfExists(err error, additionalMessage string) {
+	if err != nil {
+		if additionalMessage == "" {
+			log.Fatal(err)
+		}
+		log.Fatalf(additionalMessage, err)
+	}
+}
+
+func ensureDir(dirPath string) error {
+	if err := os.MkdirAll(dirPath, os.ModeDir); err == nil {
+		return nil
+	} else {
+		return err
+	}
+}
+
+func exists(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func isDirectory(path string) bool {
+	if info, err := os.Stat(path); err == nil && info.IsDir() {
+		return true
+	}
+	return false
+}
+
+type KatalonVersion struct {
 	Version, Filename, Os, Url, ContainingFolder string
 }
 
-func GetOS() string{
+func GetOS() string {
 	os := runtime.GOOS
 	arch := runtime.GOARCH
-	if os == "windows" {
-		if arch == "amd64" {
+	switch os {
+	case "windows":
+		if strings.Contains(arch, "64") {
 			return "windows 64"
-		} else {
-			return "windows 32"
 		}
-	} else if os == "darwin" {
+		return "windows 32"
+	case "darwin":
 		return "macos (app)"
-	} else if os == "linux" {
+	case "linux":
+		return "linux"
+	default:
 		return "linux"
 	}
-	return ""
 }
 
 func GetVersion(ksVersion string) (version KatalonVersion) {
 	resp, err := http.Get(Releases)
-	if err != nil {
-		panic(err)
-	}
+	handleErrorIfExists(err, "")
 	defer resp.Body.Close()
 
-	katalonVersions := make([]KatalonVersion,0)
+	katalonVersions := make([]KatalonVersion, 0)
 	err = json.NewDecoder(resp.Body).Decode(&katalonVersions)
-	if err != nil {
-		panic(err)
-	}
+	handleErrorIfExists(err, "")
+
 	os := GetOS()
 	for _, v := range katalonVersions {
 		if v.Version == ksVersion && strings.EqualFold(v.Os, os) {
 			version = v
-			fmt.Println(version)
 			return
 		}
 	}
 	return
+}
+
+func GetKatalonDirectory(version string) string {
+	usr, _ := user.Current()
+	home := usr.HomeDir
+
+	p := filepath.Join(home, ".katalon", version)
+	return p
+}
+
+func DownloadFile(fileUrl string, out *os.File) error {
+	// Get the data
+	resp, err := http.Get(fileUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UnzipFile(src, dest string) ([]string, error) {
+	var extractedPaths []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return extractedPaths, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		filePath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(filePath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return extractedPaths, fmt.Errorf("illegal file path: %s", filePath)
+		}
+
+		extractedPaths = append(extractedPaths, f.Name)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			err = os.MkdirAll(filePath, os.ModePerm)
+			if err != nil {
+				return extractedPaths, err
+			}
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+			return extractedPaths, err
+		}
+
+		outFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return extractedPaths, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return extractedPaths, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return extractedPaths, err
+		}
+	}
+	return extractedPaths, nil
+}
+
+func DownloadAndExtract(fileUrl, targetDir string) {
+	log.Printf("Downloading Katalon Studio from %s. It may take a few minutes.", fileUrl)
+
+	tempFile, _ := ioutil.TempFile(targetDir, "Katalon-")
+	packagePath := tempFile.Name()
+	defer tempFile.Close()
+
+	err := DownloadFile(fileUrl, tempFile)
+	handleErrorIfExists(err, "Unable to download Katalon package.")
+
+	log.Printf("Extract %s to %s", packagePath, targetDir)
+	_, err = UnzipFile(packagePath, targetDir)
+
+	handleErrorIfExists(err, fmt.Sprintf("Unable to extract %s to %s.", packagePath, targetDir))
+
+	tempFile.Close()
+	os.Remove(packagePath)
+}
+
+func GetKatalonPackage(version string) string {
+	katalonDir := GetKatalonDirectory(version)
+	err := ensureDir(katalonDir)
+	handleErrorIfExists(err, fmt.Sprintf("Unable to create directory %s to store Katalon Studio package.", katalonDir))
+
+	fileLog := filepath.Join(katalonDir, ".katalon.done")
+
+	if exists(fileLog) {
+		log.Println("Katalon Studio package has been downloaded already.")
+	} else {
+		err := os.RemoveAll(katalonDir)
+		handleErrorIfExists(err, "")
+
+		err = ensureDir(katalonDir)
+		handleErrorIfExists(err, fmt.Sprintf("Unable to create directory %s to store Katalon Studio package.", katalonDir))
+
+		katalonVersion := GetVersion(version)
+		versionUrl := katalonVersion.Url
+
+		DownloadAndExtract(versionUrl, katalonDir)
+		_, err = os.Create(fileLog)
+		handleErrorIfExists(err, "")
+		log.Println("Katalon Studio has been installed successfully.")
+	}
+
+	katalonContainingDir := ""
+	childrenNames, err := ioutil.ReadDir(katalonDir)
+	handleErrorIfExists(err, "")
+
+	for _, childrenName := range childrenNames {
+		fullPath := filepath.Join(katalonDir, childrenName.Name())
+
+		if isDirectory(fullPath) && strings.Contains(childrenName.Name(), "Katalon") {
+			katalonContainingDir = fullPath
+			break
+		}
+	}
+
+	return katalonContainingDir
 }
--- src/com/katalon/katalonwrapper/main.go
+++ src/com/katalon/katalonwrapper/main.go
@@ -1,11 +1,14 @@
 package main
 
-import ( 
-  "com/katalon/katalonwrapper/download"
-  "os"
+import (
+	"com/katalon/katalonwrapper/download"
+	"fmt"
 )
 
 func main() {
-    ksVersion := os.Args[1]
-    download.GetVersion(ksVersion)
+	//ksVersion := os.Args[1]
+	//download.GetVersion(ksVersion)
+	var version = "6.3.3"
+	katalonDir := download.GetKatalonPackage(version)
+	fmt.Println("Katalon Directory:", katalonDir)
 }
